2062b5c8b03c5b8b3f87104f3cf40dbad080f7fd
Narenyadav0929 <Narenyadav0929@gmail.com>
2025-12-21 13:54:50 +0530

    Traning Pipeline

---
+++ networksecurity/pipeline/training_pipeline.py
@@ -0,0 +1,72 @@
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+import sys
+import os
+from networksecurity.entity.config_entity import (DataIngestionConfig, TrainingPipelineConfig,
+                                                   DataValidationConfig, DataTransformationConfig,
+                                                   ModelTrainerConfig)
+
+from networksecurity.entity.artifact_entity import (
+    DataIngestionArtifact,
+    DataValidationArtifact,
+    DataTransformationArtifact)
+
+
+class TraningPipeline:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.training_pipeline_config = training_pipeline_config
+
+    def start_data_ingetion(self):
+        try:    
+            self.data_ingestion_config = DataIngestionConfig(self.training_pipeline_config)
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_injetion()
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
+        try:
+            self.data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_injetion_artifact=data_ingestion_artifact,
+                                                data_validation_config=self.data_validation_config)
+            data_validation_artifacts = data_validation.initiate_data_validation()
+            return data_validation_artifacts
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+
+    def start_data_transformation(self,data_validation_artifacts:DataValidationArtifact):
+        try:
+            self.data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifacts,
+                                        data_transformation_config=self.data_transformation_config)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    
+    def start_model_trainer(self, data_transformation_artifact : DataTransformationArtifact):
+        try:
+            self.model_trainer_config=ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+            model_trainer = ModelTrainer(model_trainer_config=self.model_trainer_config,
+                                data_transformation_artifact= data_transformation_artifact)
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+
+        
+    def run_pipeline(self):
+        data_injetion_artifact = self.start_data_ingetion()
+        data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_injetion_artifact)
+        data_transformation_artifact = self.start_data_transformation(data_validation_artifacts=data_validation_artifact)
+        model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+        return model_trainer_artifact
