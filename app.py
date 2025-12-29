059c33ca961856b01f8e9c79c0fbcb60c0edb9e4
Narenyadav0929 <Narenyadav0929@gmail.com>
2025-12-29 10:06:42 +0530

    Final changes for deployment

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ async def train_route():
     except Exception as e:
         raise NetworkSecurityException(e, sys)
     
+    
 
 @app.post("/predict")
 async def predict_route(request: Request,file: UploadFile = File(...)):
