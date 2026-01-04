c301974593d073bfd6e6ae6d1fcd3eb1e48b77f1
Narenyadav0929 <Narenyadav0929@gmail.com>
2026-01-04 12:33:05 +0530

    ensure API starts correctly inside Docker container

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -75,7 +75,7 @@ class ModelTrainer:
             }
         params={
             "Decision Tree": {
-                'criterion':['gini', 'entropy', 'log_loss'],
+                'criterion':['gini']#, 'entropy', 'log_loss'],
                 # 'splitter':['best','random'],
                 # 'max_features':['sqrt','log2'],
             },
@@ -87,20 +87,20 @@ class ModelTrainer:
             },
             "Gradient Boosting":{
                 # 'loss':['log_loss', 'exponential'],
-                'learning_rate':[.1,.01,.05,.001],
-                'subsample':[0.6,0.7,0.75,0.85,0.9],
+                'learning_rate':[.1,.01]#,.05,.001],
+                # 'subsample':[0.6,0.7,0.75,0.85,0.9],
                 # 'criterion':['squared_error', 'friedman_mse'],
                 # 'max_features':['auto','sqrt','log2'],
-                'n_estimators': [8,16,32,64,128,256]
+                # 'n_estimators': [8,16,32,64,128,256]
             },
             "Logistic Regression":{},
             "AdaBoost":{
-                'learning_rate':[.1,.01,.001],
-                'n_estimators': [8,16,32,64,128,256]
+                'learning_rate':[.1,.01]#,.001],
+                # 'n_estimators': [8,16,32,64,128,256]
             },
             "KNeighbors Classifier": {
                 'n_neighbors': [4,5,8,10],
-                'algorithm' : ['auto', 'ball_tree', 'kd_tree']
+                # 'algorithm' : ['auto', 'ball_tree', 'kd_tree']
             }
             
         }
